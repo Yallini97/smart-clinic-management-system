89132ad62e09a3d2656736f4c479dd9225556723
YALLINI CHANDER <s2039611@siswa.um.edu.my>
2025-07-08 17:52:29 +0800

    Added DoctorService.java for doctor CRUD operations

---
+++ backend/src/main/java/com/clinic/services/DoctorService.java
@@ -0,0 +1,32 @@
+package com.clinic.services;
+
+import com.clinic.models.Doctor;
+import com.clinic.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Optional<Doctor> getDoctorById(Long id) {
+        return doctorRepository.findById(id);
+    }
+
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
+}
